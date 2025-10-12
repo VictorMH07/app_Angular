db5d86456f38647ee12f4fb37bfbeb3221ae97e2
Victor Hernandez <victor160700@gmail.com>
2025-10-12 17:36:19 -0500

    Inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './components/inicio/inicio';
+import { Login } from './components/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Inicio },
+    { path: 'login', component: Login }
+];
