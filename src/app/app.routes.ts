5006d94ecf5b325cc878bba74c60972a191a0dfc
Victor Hernandez <victor160700@gmail.com>
2025-10-16 19:00:28 -0500

    Implementacion del catalogo V1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './components/inicio/inicio';
 import { Login } from './components/login/login';
+import { Catalogo } from './components/catalogo/catalogo';
 
 export const routes: Routes = [
     { path: '', component: Inicio },
-    { path: 'login', component: Login }
+    { path: 'login', component: Login },
+    { path: 'catalogo', component: Catalogo }
 ];
